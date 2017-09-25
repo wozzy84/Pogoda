ffd025503c6b676c9d3316a2f360c078a5a3bb19
wozzy84 <piotrw84@gmail.com>
2017-09-25 20:47:16 +0200

    js clean

--- assets/app.js
+++ assets/app.js
@@ -1,52 +1,59 @@
 $(function() {
- miasta = "";
- $("#wykresTemperatury").hide();
- $("#wykresCisnienia").hide();
+    miasta = "";
+    $("#wykresTemperatury").hide();
+    $("#wykresCisnienia").hide();
 
-  $('input, select').on('change', function(event) {
-    var $element = $(event.target),
-      $container = $element.closest('.example');
+    $('input, select').on('change', function(event) {
+        var $element = $(event.target),
+            $container = $element.closest('.example');
 
-    if (!$element.data('tagsinput'))
-      return;
+        if (!$element.data('tagsinput'))
+            return;
 
-    var val = $element.val();
-    if (val === null)
-      val = "null";
-    $('code', $('pre.val', $container)).html( ($.isArray(val) ? JSON.stringify(val) : "\"" + val.replace('"', '\\"') + "\"") );
-    $('code', $('pre.items', $container)).html(JSON.stringify($element.tagsinput('items')));
-    miasta = $element.val();
-    console.log(miasta)
-
-
-  }).trigger('change');
+        var val = $element.val();
+        if (val === null)
+            val = "null";
+        $('code', $('pre.val', $container)).html(($.isArray(val) ? JSON.stringify(val) : "\"" + val.replace('"', '\\"') + "\""));
+        $('code', $('pre.items', $container)).html(JSON.stringify($element.tagsinput('items')));
+        miasta = $element.val();
+        console.log(miasta)
 
 
+    }).trigger('change');
 
 
-$("#generuj").on('click', function(event)
-{ 
-
- $("#temperatura").attr('src','http://127.0.0.1:3000/temperaturePlot?cities='+miasta);
- $("#cisnienie").attr('src','http://127.0.0.1:3000/pressurePlot?cities='+miasta);
- $('#miastaTekst span').text(miasta.replace(/,/g, ", "));
 
 
-  if($("#tempSwitchOptionSuccess").is(":checked")){
-    $("#wykresTemperatury").show(); console.log("checked")
-    }
-    else { console.log("unchecked");$("#wykresTemperatury").hide(); }
- 
+    $("#generuj").on('click', function(event) {
+
+        $("#temperatura").attr('src', 'http://127.0.0.1:3000/temperaturePlot?cities=' + miasta);
+        $("#cisnienie").attr('src', 'http://127.0.0.1:3000/pressurePlot?cities=' + miasta);
+        $('#miastaTekst span').text(miasta.replace(/,/g, ", "));
 
 
-  if($("#pressureSwitchOptionInfo").is(":checked")){
-    $("#wykresCisnienia").show(); console.log("checked")
-    }
-    else { console.log("unchecked");$("#wykresCisnienia").hide(); }
+        if ($("#tempSwitchOptionSuccess").is(":checked")) {
+            $("#wykresTemperatury").show();
+            console.log("checked")
+        } else {
+            console.log("unchecked");
+            $("#wykresTemperatury").hide();
+        }
 
-$('html, body').animate({scrollTop:$(document).height()}, 'slow'); 
-  
 
-})
 
-});     
+        if ($("#pressureSwitchOptionInfo").is(":checked")) {
+            $("#wykresCisnienia").show();
+            console.log("checked")
+        } else {
+            console.log("unchecked");
+            $("#wykresCisnienia").hide();
+        }
+
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 'slow');
+
+
+    })
+
+});
